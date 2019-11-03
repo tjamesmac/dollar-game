b724e280ac3f66c1d2b17e58df005ad0bbf980d2
tjamesmac <tjamesmac@gmail.com>
2019-11-03 21:44:14 +0000

    Add webpack config

---
+++ util/webpack.common.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/client/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/client/assets/index.html"
+    })
+  ],
+  output: {
+    filename: "bundle.[chunkhash:8].js",
+    path: path.resolve(__dirname, "../public"),
+    publicPath: "/"
+  }
+}
