1cb6652a9116b1aab8d8a07535ab462b1321b4cc
tjamesmac <tjamesmac@gmail.com>
2019-11-04 13:33:29 +0000

    Add rules to config

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,10 +20,44 @@ module.exports = {
         },
         "ecmaVersion": 2018
     },
+    "settings": {
+        "react": {
+            "version": "detect"
+        },
+    },
     "plugins": [
-        "react"
+        "react",
+        "react-hooks"
     ],
     "rules": {
-        "quotes": [2, "double", { "avoidEscape": true }]
+        "quotes": [2, "double", { "avoidEscape": true }],
+        "block-spacing": [2, "always"],
+        "brace-style": [2, "1tbs"],
+        "comma-dangle": ["error", "always"],
+        "eol-last": [2, "always"],
+        "indent": [2, 4],
+        "jsx-quotes": [2, "prefer-double"],
+        "no-trailing-spaces": [2, { "skipBlankLines": true }],
+        "no-var": [2],
+        "prefer-destructuring": ["error", {
+            "VariableDeclarator": {
+              "array": false,
+              "object": true
+            },
+            "AssignmentExpression": {
+              "array": true,
+              "object": true
+            }
+          },
+          {
+            "enforceForRenamedProperties": false
+          },
+        ],
+        "sort-imports": ["error", {
+            "ignoreCase": false,
+            "ignoreDeclarationSort": false,
+            "ignoreMemberSort": false,
+            "memberSyntaxSortOrder": ["none", "all", "multiple", "single"]
+        }]
     }
-};
+}
