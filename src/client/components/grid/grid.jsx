fc104b3734d2aad98024004b3263ddf9b24de673
tjamesmac <tjamesmac@gmail.com>
2019-11-05 19:08:26 +0000

    Correct names

--- src/client/components/grid/grid.jsx
+++ src/client/components/grid/grid.jsx
@@ -3,25 +3,26 @@ import Node from "../node/node.jsx";
 import React from "react";
 
 function Grid(props) {
+    // generate bridges = bridges > 1 && bridges < nodes.length - 1
     const { rowNumber, colNumber, nodeNumber } = props;
     console.log(nodeNumber);
     const row = [...Array(rowNumber).keys()];
-    const col = [...Array(colNumber).keys()];
+    const columns = [...Array(colNumber).keys()];
     const gridArray = [];
-    const targetNode = [[1, 3], [2, 2], [4, 4]];
-    for (const item of col) {
+    const nodes = [[1, 3], [2, 2], [4, 4]];
+    for (const col of columns) {
         const rowArray = [];
-        for (const i of row) {
+        for (const item of row) {
             const obj = {};
-            obj.index = i;
+            obj.index = item;
             obj.active = false;
             rowArray.push(obj);
         }
         gridArray.push(rowArray);
     }
-    for (const node of targetNode) {
-        for (const [i, item] of gridArray.entries()) {
-            if (node[0] === i) {
+    for (const node of nodes) {
+        for (const [index, item] of gridArray.entries()) {
+            if (node[0] === index) {
                 console.log("hello");
                 for (const iter of item) {
                     if (node[1] === iter.index) {
