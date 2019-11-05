88aea70d2543c44b3465b58c4444d6cd8c14f8ff
tjamesmac <tjamesmac@gmail.com>
2019-11-05 19:17:19 +0000

    Tidy up grid

--- src/client/components/grid/grid.jsx
+++ src/client/components/grid/grid.jsx
@@ -1,29 +1,21 @@
 import "./grid.scss";
 import Node from "../node/node.jsx";
 import React from "react";
+import { generateGridArr } from "./grid.helpers.jsx";
 
 function Grid(props) {
     // generate bridges = bridges > 1 && bridges < nodes.length - 1
     const { rowNumber, colNumber, nodeNumber } = props;
-    console.log(nodeNumber);
-    const row = [...Array(rowNumber).keys()];
-    const columns = [...Array(colNumber).keys()];
-    const gridArray = [];
+
+    const rowLength = [...Array(rowNumber).keys()];
+    const columnsLength = [...Array(colNumber).keys()];
     const nodes = [[1, 3], [2, 2], [4, 4]];
-    for (const col of columns) {
-        const rowArray = [];
-        for (const item of row) {
-            const obj = {};
-            obj.index = item;
-            obj.active = false;
-            rowArray.push(obj);
-        }
-        gridArray.push(rowArray);
-    }
+
+    const gridArray = generateGridArr(rowLength, columnsLength); // returns array that represents grid
+
     for (const node of nodes) {
         for (const [index, item] of gridArray.entries()) {
             if (node[0] === index) {
-                console.log("hello");
                 for (const iter of item) {
                     if (node[1] === iter.index) {
                         iter.active = true
@@ -32,10 +24,7 @@ function Grid(props) {
             }
         }
     }
-    console.log(gridArray);
-
     const gridMaker = gridArray.map((item, index) => {
-        console.log(item);
         const rowMaker = item.map((row, rowIndex) => {
             if (row.active) {
                 return (
@@ -46,16 +35,13 @@ function Grid(props) {
                     <div className="row" key={rowIndex}>{rowIndex}</div>
                 )
             }
-          
         })
         return (
             <div className="col" key={index}>{rowMaker}</div>
         )
     })
-  
     return(
         <div className="grid-container">
-            grid go here
             {gridMaker}
         </div>
     )
