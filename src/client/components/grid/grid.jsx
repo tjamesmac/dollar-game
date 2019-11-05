067b5a6d7e2ff8e0dea6091566f0d0a0efef944a
tjamesmac <tjamesmac@gmail.com>
2019-11-05 17:31:20 +0000

    Add grid

--- src/client/components/grid/grid.jsx
+++ src/client/components/grid/grid.jsx
@@ -2,24 +2,59 @@ import "./grid.scss";
 import React from "react";
 
 function Grid(props) {
-    const { rowNumber, colNumber } = props;
+    const { rowNumber, colNumber, nodeNumber } = props;
+    console.log(nodeNumber);
     const row = [...Array(rowNumber).keys()];
     const col = [...Array(colNumber).keys()];
-    const rowDivs = row.map((item, index) => {
+    const gridArray = [];
+    const targetNode = [[1, 3], [2, 2], [4, 4]];
+    for (const item of col) {
+        const rowArray = [];
+        for (const i of row) {
+            const obj = {};
+            obj.index = i;
+            obj.active = false;
+            rowArray.push(obj);
+        }
+        gridArray.push(rowArray);
+    }
+    for (const node of targetNode) {
+        for (const [i, item] of gridArray.entries()) {
+            if (node[0] === i) {
+                console.log("hello");
+                for (const iter of item) {
+                    if (node[1] === iter.index) {
+                        iter.active = true
+                    }
+                }
+            }
+        }
+    }
+    console.log(gridArray);
+
+    const gridMaker = gridArray.map((item, index) => {
+        console.log(item);
+        const rowMaker = item.map((row, rowIndex) => {
+            if (row.active) {
+                return (
+                    <div className="row" key={rowIndex}>Active</div>
+                )
+            } else {
+                return (
+                    <div className="row" key={rowIndex}>{rowIndex}</div>
+                )
+            }
+          
+        })
         return (
-            <div className="row" key={index}>{index}</div>
-        )
-    });
-    const colDivs = col.map((item, index) => {
-        return (
-            <div className="col" key={index}>
-                {rowDivs}
-            </div>
+            <div className="col" key={index}>{rowMaker}</div>
         )
     })
+  
     return(
         <div className="grid-container">
-            {colDivs}
+            grid go here
+            {gridMaker}
         </div>
     )
 }
