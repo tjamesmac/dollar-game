ca3cd0ca712802e916df81d8f3e042ffb0bae3fe
tjamesmac <tjamesmac@gmail.com>
2019-11-05 14:51:52 +0000

    Add props and handle click

--- src/client/components/node/node.jsx
+++ src/client/components/node/node.jsx
@@ -1,10 +1,14 @@
 import "./node.scss";
 import React from "react";
 
-function Node() {
+function Node(props) {
+    const { name } = props;
 
+    function handleClick() {
+        console.log(name);
+    }
     return (
-        <div className="node">
+        <div className="node" onClick={handleClick}>
         </div>
     )
 }
