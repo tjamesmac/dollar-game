cfafde30f69dfa2332503a9a3f6420cc9ec35686
tjamesmac <tjamesmac@gmail.com>
2019-11-07 21:24:15 +0000

    Remove unwanted stuff

--- src/client/components/app/app.jsx
+++ src/client/components/app/app.jsx
@@ -1,17 +1,12 @@
 import Grid from "../grid/grid.jsx"
-import Node from "../node/node.jsx"
 import React from "react";
 
 function App() {
-    const [coordinates, setCoordinates] = React.useState(null);
-    // line = <svg height="500" width="500" style={svgStyle}>
-    //     <line x1={x1} y1={5} x2="61" y2="224" style={lineStyle} />
-    // </svg>
+
     return (
         <div>
             Hello world!
             <Grid colNumber={5} rowNumber={5} nodeNumber={5} />
-            {/* {line} */}
         </div>
     )
 }
