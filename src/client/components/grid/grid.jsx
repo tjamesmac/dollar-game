e90af51096f0ddd063a15368a88d3d190f75fcdf
tjamesmac <tjamesmac@gmail.com>
2019-11-07 21:24:22 +0000

    Add getCoordinates

--- src/client/components/grid/grid.jsx
+++ src/client/components/grid/grid.jsx
@@ -4,6 +4,8 @@ import Node from "../node/node.jsx";
 import React from "react";
 
 function Grid(props) {
+    const [coordinates, setCoordinates] = React.useState(null);
+    const [x, y] = React.useState(null);
     // generate bridges = bridges > 1 && bridges < nodes.length - 1
     const { rowNumber, colNumber, nodeNumber } = props;
 
@@ -12,7 +14,23 @@ function Grid(props) {
     const nodeLength = [...Array(nodeNumber).keys()];
 
     const gridArray = generateGridArr(rowLength, columnsLength); // returns array that represents grid
+    
+    const coorArr = [];
+    function getNodeCoordinates() {
+        console.log("hello I am coordinates");
+        const nodes = document.querySelectorAll(".node");
+        console.log(nodes);
+        for (const item of nodes) {
+            const pos = item.getBoundingClientRect();
+            coorArr.push(pos);
+        }
+        console.log(coorArr);
+        setCoordinates(coorArr);
+    }
     const nodeCordinates = generateNodeCoordinates(nodeLength, rowNumber, colNumber);
+    React.useEffect(() => {
+        getNodeCoordinates()
+    }, []);
 
     
 
@@ -22,7 +40,6 @@ function Grid(props) {
             if (node[0] === index) {
                 for (const iter of item) {
                     if (node[1] === iter.index) {
-                        console.log(count, "count");
                         iter.active = true
                         iter._id = count;
                         count += 1;
@@ -47,8 +64,23 @@ function Grid(props) {
             <div className="col" key={index}>{rowMaker}</div>
         )
     })
+
+    let line;
+    if (coordinates) {
+        const svgStyle = {
+            position: "absolute",
+        }
+        const lineStyle = {
+            stroke: "black",
+        }
+        console.log(coordinates);
+        line = <svg height="500" width="500" style={svgStyle}>
+            <line x1={coordinates[0].x} y1={coordinates[0].y} x2={coordinates[1].x} y2={coordinates[1].y} style={lineStyle}/>
+        </svg>
+    }
     return(
         <div className="grid-container">
+            {line}
             {gridMaker}
         </div>
     )
