7570ddfb2e230cf5df58ae413db4590c04adcc75
tjamesmac <tjamesmac@gmail.com>
2019-11-07 19:53:39 +0000

    Add line example

--- src/client/components/app/app.jsx
+++ src/client/components/app/app.jsx
@@ -3,15 +3,15 @@ import Node from "../node/node.jsx"
 import React from "react";
 
 function App() {
-    let a = 2;
-    if (a == 2) {
-      //  console.log(a);
-    }
+    const [coordinates, setCoordinates] = React.useState(null);
+    // line = <svg height="500" width="500" style={svgStyle}>
+    //     <line x1={x1} y1={5} x2="61" y2="224" style={lineStyle} />
+    // </svg>
     return (
         <div>
             Hello world!
-            <Node name="fred" />
             <Grid colNumber={5} rowNumber={5} nodeNumber={5} />
+            {/* {line} */}
         </div>
     )
 }
