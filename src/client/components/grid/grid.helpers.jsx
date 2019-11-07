179cef77e6360bd9f43af4b0435a2dff488101d7
tjamesmac <tjamesmac@gmail.com>
2019-11-07 16:35:28 +0000

    Add bridge generator

--- src/client/components/grid/grid.helpers.jsx
+++ src/client/components/grid/grid.helpers.jsx
@@ -16,8 +16,8 @@ export function generateGridArr(rows, columns) {
 
 // generates random numbers between the values we want
 export function generateNodeCoordinates(nodeLength, rowNumber, colNumber) {
-    const coordinateArray = []; // represent
-    function generateRandomCoordinate() {
+    const coordinateArray = [];
+    function generateRandomCoordinate() { // get randomised coordinate array
         function getRandomIntMinMax(min, max) {
             min = Math.ceil(min);
             max = Math.floor(max);
@@ -26,21 +26,23 @@ export function generateNodeCoordinates(nodeLength, rowNumber, colNumber) {
         const randomCoordinates = [getRandomIntMinMax(0, rowNumber), getRandomIntMinMax(0, colNumber)]; // amount of rows - columns
         return randomCoordinates;
     }
-    function checkUniqueValues(coordinateArrayCheck) {
-        const uniquePairs = pairs => [...new Set(pairs.map(pair => JSON.stringify(pair)))].map(pair => JSON.parse(pair));
+    function checkUniqueValues(coordinateArrayCheck) { //
+        const uniquePairs = pairs => [...new Set(pairs.map(pair => JSON.stringify(pair)))].map(pair => JSON.parse(pair)); // converts to a JSON and pushed into a set
         const uniqueCoordinates = uniquePairs(coordinateArrayCheck);
-        console.log(uniqueCoordinates.length, nodeLength.length)
+
         if (uniqueCoordinates.length === nodeLength.length) {
             console.log("Coordinates complete and are unique");
             return uniqueCoordinates;
         } else {
             console.log("not unique");
+
             // generate coordinate
             const extraCoordinates = generateRandomCoordinate();
-            console.log(extraCoordinates);
+
             // push into array
             uniqueCoordinates.push(extraCoordinates);
-            // call checkUniqueValue(array);`
+
+            // call checkUniqueValue(array);
             return checkUniqueValues(uniqueCoordinates);
         }
     }
@@ -52,6 +54,21 @@ export function generateNodeCoordinates(nodeLength, rowNumber, colNumber) {
     }
     const validCoordinates = checkUniqueValues(coordinateArray);
     return validCoordinates;
+}
+
+export function generateBridges(nodeLength) {
+    const maximumBridges = nodeLength - 1;
+    function getRandomIntMinMax(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+    }
+    const randomBridgeLength = getRandomIntMinMax(1, maximumBridges);
+    const randomArray = (length, max) => [...new Array(length)]
+        .map(() => Math.floor(Math.random() * (max - 1)) + 1);
+    const randomValues = randomArray(randomBridgeLength, maximumBridges);
+    const filteredArray = [...new Set(randomValues)]
+    return filteredArray;
 }
 
 
