662a529ce4d8369eccf90ea41a66c9f571516d0d
tjamesmac <tjamesmac@gmail.com>
2019-11-08 22:13:13 +0000

    Change back to value

--- src/client/components/node/node.jsx
+++ src/client/components/node/node.jsx
@@ -2,13 +2,17 @@ import "./node.scss";
 import React from "react";
 
 function Node(props) {
-    const { id, value } = props;
 
+    const { id, value, clickHandler } = props;
+    const [nodeValue, setNodeValue] = React.useState(5);
+    
     function handleClick() {
+        setNodeValue(nodeValue - 1);
         console.log(value);
+        
     }
     return (
-        <div id={id} className="node" onClick={handleClick}>
+        <div id={id} className="node" onClick={clickHandler}>
             {value}
         </div>
     )
