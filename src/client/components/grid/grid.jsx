437d3f0cb4a6bcfb2a574903b9a0ce6760a50d73
tjamesmac <tjamesmac@gmail.com>
2019-11-08 16:15:36 +0000

    Separate logic into own components

--- src/client/components/grid/grid.jsx
+++ src/client/components/grid/grid.jsx
@@ -1,87 +1,24 @@
 import "./grid.scss";
-import { generateBridges, generateGridArr, generateNodeCoordinates } from "./grid.helpers.jsx";
-import Node from "../node/node.jsx";
+import GridItem from "../gridItem/gridItem.jsx";
+import Line from "../line/line.jsx";
 import React from "react";
 
+// collect all 'grid' related items and put into a new component
+// collect all line related items and put into a new component
+// treat them separately
+
+
 function Grid(props) {
-    const [coordinates, setCoordinates] = React.useState(null);
-    const [x, y] = React.useState(null);
     // generate bridges = bridges > 1 && bridges < nodes.length - 1
     const { rowNumber, colNumber, nodeNumber } = props;
-
     const rowLength = [...Array(rowNumber).keys()];
     const columnsLength = [...Array(colNumber).keys()];
     const nodeLength = [...Array(nodeNumber).keys()];
 
-    const gridArray = generateGridArr(rowLength, columnsLength); // returns array that represents grid
-    
-    const coorArr = [];
-    function getNodeCoordinates() {
-        console.log("hello I am coordinates");
-        const nodes = document.querySelectorAll(".node");
-        console.log(nodes);
-        for (const item of nodes) {
-            const pos = item.getBoundingClientRect();
-            coorArr.push(pos);
-        }
-        console.log(coorArr);
-        setCoordinates(coorArr);
-    }
-    const nodeCordinates = generateNodeCoordinates(nodeLength, rowNumber, colNumber);
-    React.useEffect(() => {
-        getNodeCoordinates()
-    }, []);
-
-    
-
-    let count = 0;
-    for (const node of nodeCordinates) {
-        for (const [index, item] of gridArray.entries()) {
-            if (node[0] === index) {
-                for (const iter of item) {
-                    if (node[1] === iter.index) {
-                        iter.active = true
-                        iter._id = count;
-                        count += 1;
-                    }
-                }
-            }
-        }
-    }
-    const gridMaker = gridArray.map((item, index) => {
-        const rowMaker = item.map((row, rowIndex) => {
-            if (row.active) {
-                return (
-                    <div className="row" key={rowIndex}><Node id={row._id} bridges={generateBridges(nodeLength.length)} /></div>
-                )
-            } else {
-                return (
-                    <div className="row" key={rowIndex}>{rowIndex}</div>
-                )
-            }
-        })
-        return (
-            <div className="col" key={index}>{rowMaker}</div>
-        )
-    })
-
-    let line;
-    if (coordinates) {
-        const svgStyle = {
-            position: "absolute",
-        }
-        const lineStyle = {
-            stroke: "black",
-        }
-        console.log(coordinates);
-        line = <svg height="500" width="500" style={svgStyle}>
-            <line x1={coordinates[0].x} y1={coordinates[0].y} x2={coordinates[1].x} y2={coordinates[1].y} style={lineStyle}/>
-        </svg>
-    }
-    return(
+    return (
         <div className="grid-container">
-            {line}
-            {gridMaker}
+            <GridItem rowLength={rowLength} columnsLength={columnsLength} nodeLength={nodeLength} />
+            <Line />
         </div>
     )
 }
