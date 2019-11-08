65d8d092d3bd6fb8823cd7da936f02f73990a61c
tjamesmac <tjamesmac@gmail.com>
2019-11-08 20:15:46 +0000

    Add gridController

---
+++ src/client/components/gridController/gridController.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function GridController() {
+    return (
+        <React.Fragment>
+            <div>hello</div>
+        </React.Fragment>
+    )
+}
+
+export default GridController;
