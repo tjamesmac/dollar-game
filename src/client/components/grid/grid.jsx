67accb6fca30d6d93243f576a0d18f22ca051776
tjamesmac <tjamesmac@gmail.com>
2019-11-09 12:58:55 +0000

    Generate nodes in grid

--- src/client/components/grid/grid.jsx
+++ src/client/components/grid/grid.jsx
@@ -1,5 +1,5 @@
 import "./grid.scss";
-import { generateBridges, generateRandomArray } from "./grid.helpers.jsx";
+import { generateBridges, generateGridArr, generateNodeCoordinates, generateRandomArray } from "./grid.helpers.jsx";
 import GridItem from "../gridItem/gridItem.jsx";
 import Line from "../line/line.jsx";
 import React from "react";
@@ -30,9 +30,21 @@ function Grid(props) {
         totalBridges += bridge.length;
     }
     const nodeValueArray = generateRandomArray(nodeNumber, totalBridges);
+    const gridArray = generateGridArr(rowLength, columnsLength); // returns array that represents grid
+        
+    const nodeCordinates = generateNodeCoordinates(nodeLength, rowLength.length, columnsLength.length);
+
     return (
         <div className="grid-container">
-            <GridItem rowLength={rowLength} columnsLength={columnsLength} nodeLength={nodeLength} nodeValues={nodeValueArray} bridges={bridgeArray}/>
+            <GridItem
+                rowLength={rowLength}
+                columnsLength={columnsLength}
+                nodeLength={nodeLength}
+                nodeValues={nodeValueArray}
+                bridges={bridgeArray}
+                gridArr={gridArray}
+                nodeCoor={nodeCordinates}
+            />
             <Line bridges={bridgeArray} />
         </div>
     )
