8017ac7d839450da17d69da0dcd0576faf1ec4d1
tjamesmac <tjamesmac@gmail.com>
2019-11-09 12:58:21 +0000

    Update node

--- src/client/components/node/node.jsx
+++ src/client/components/node/node.jsx
@@ -3,19 +3,11 @@ import React from "react";
 
 function Node(props) {
 
-    const { id, value, clickHandler } = props;
-    function handleClick(ID, VAL) {
-        console.log(VAL, "VAL")
-        console.log(ID, "ID")
-        clickHandler(ID, VAL);
-        // setNodeValue(nodeValue - 1);
-    }
-
+    const { id, value } = props;
     return (
-        // <div id={id} className="node" onClick={() => clickHandler(id, value)}>
-        // <div id={id} className="node" onClick={() => handleClick(id, value)}>
         <div id={id} className="node">
             {value}
+            id: {id}
         </div>
     )
 }
