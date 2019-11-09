7f59f37408fc24e6ebcb4c07b6f684fb4b50b657
tjamesmac <tjamesmac@gmail.com>
2019-11-09 00:47:04 +0000

    Remove comments

--- src/client/components/grid/grid.jsx
+++ src/client/components/grid/grid.jsx
@@ -4,16 +4,7 @@ import GridItem from "../gridItem/gridItem.jsx";
 import Line from "../line/line.jsx";
 import React from "react";
 
-// randomly generate x numbers in array and x = nodelength
-// the array must equal the bridges - nodes + 1 or greater
-// TODO: get number of bridges
-// generate random array - 9 / 9 [1, -4, 5]
-// add contents of array
-// if not equal or greater, do again until equal
-
 function Grid(props) {
-    // generate bridges = bridges > 1 && bridges < nodes.length - 1
-    const [generatedNodeValues, setNodeValues] = React.useState(null);
     const { rowNumber, colNumber, nodeNumber } = props;
     const rowLength = [...Array(rowNumber).keys()];
     const columnsLength = [...Array(colNumber).keys()];
