9230ef880efd38f0ddf7073295ae2917d24f0fec
tjamesmac <tjamesmac@gmail.com>
2019-11-12 13:55:36 +0000

    Revert number of nodes

--- src/client/components/app/app.jsx
+++ src/client/components/app/app.jsx
@@ -7,7 +7,7 @@ function App() {
         <div>
 
             <span className="bold-text">The Dollar Game!</span>
-            <GridContainer colNumber={10} rowNumber={10} nodeNumber={100} />
+            <GridContainer colNumber={10} rowNumber={10} nodeNumber={5} />
 
         </div>
     )
