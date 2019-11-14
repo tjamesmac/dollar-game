1f5a941e29b4117d0937e42d895ceed620298bc7
tjamesmac <tjamesmac@gmail.com>
2019-11-14 14:17:59 +0000

    Remove comment

--- src/client/components/node/node.jsx
+++ src/client/components/node/node.jsx
@@ -3,14 +3,7 @@ import React from "react";
 
 function Node(props) {
     const { id, value } = props;
-    // const style = {
-    //     height: 50 + value,
-    //     width: 50 + value,
-    //     minHeight: "40px",
-    //     minWidth: "40px",
-    //     maxHeight: "40px",
-    //     maxWidth:  "40px",
-    // }
+
     return (
         <div id={id} className="node">
             {value}
