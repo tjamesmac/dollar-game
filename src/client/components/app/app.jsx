6ca6e6de51931fccc5126633e5035940139fffd3
tjamesmac <tjamesmac@gmail.com>
2019-11-14 17:54:52 +0000

    Change nodes, rows, cols

--- src/client/components/app/app.jsx
+++ src/client/components/app/app.jsx
@@ -2,9 +2,9 @@ import Grid from "../grid/grid.jsx"
 import React from "react";
 
 function App() {
-    const columns = 15;
-    const rows = 15;
-    const nodes = 5
+    const columns = 5;
+    const rows = 5;
+    const nodes = 5;
     return (
         <>
             <span className="bold-text">The Dollar Game!</span>
