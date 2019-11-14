fa269f0d214c7304ad581adfd1a9210c7839cec6
tjamesmac <tjamesmac@gmail.com>
2019-11-14 11:42:04 +0000

    Replace values with variables

--- src/client/components/app/app.jsx
+++ src/client/components/app/app.jsx
@@ -2,11 +2,13 @@ import Grid from "../grid/grid.jsx"
 import React from "react";
 
 function App() {
-
+    const columns = 15;
+    const rows = 15;
+    const nodes = 5
     return (
         <>
             <h1 className="bold-text">The Dollar Game!</h1>
-            <Grid columns={15} rows={15} nodes={5} />
+            <Grid columns={columns} rows={rows} nodes={nodes} />
         </>
     )
 }
